f63ef9478ca24c23b213acc0bbfb8821f7c45596
Mixas75 <michelmanhaes@gmail.com>
2022-04-14 09:50:23 -0300

    Comandos iniciais em PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    
+    <head>
+        <title>
+        Meu primeiro site em PHP! Será?
+        </title>
+    </head>
+
+    <body>
+        <?php
+            for ($i=0; $i <10 ; $i++) {
+                print ( "Linha número " . $i . "<br />");
+            }
+            ?>
+    </body>
+</html>
